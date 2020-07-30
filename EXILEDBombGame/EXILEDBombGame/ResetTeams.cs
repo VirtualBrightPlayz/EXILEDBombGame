708f7b0f7096f0281d2468e18b8d31f4b16a00d1
VirtualBrightPlayz <lego.trains.b@gmail.com>
2020-07-30 11:17:39 -0700

    reset teams

---
+++ EXILEDBombGame/EXILEDBombGame/ResetTeams.cs
@@ -0,0 +1,27 @@
+﻿using CommandSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXILEDBombGame
+{
+    [CommandHandler(typeof(RemoteAdminCommandHandler))]
+    public class ResetTeams : ICommand
+    {
+        public string Command => "resetteams";
+
+        public string[] Aliases => new string[0];
+
+        public string Description => "Resets the team list";
+
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            PluginMain.instance.PLEV.CIPlayers.Clear();
+            PluginMain.instance.PLEV.NTFPlayers.Clear();
+            response = "Teams reset.";
+            return true;
+        }
+    }
+}
